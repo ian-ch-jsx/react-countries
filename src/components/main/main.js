bce3658d598722f2ba5b9c7a01f1afb1f8c409fa
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-07 15:45:33 -0800

    can filter by continent.

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -18,30 +18,31 @@ export default function Main() {
 
   function filterCountries() {
     return country.filter((c) => {
-      return c.name.includes(query);
+      return c.name.includes(query) && (c.continent === continent || continent === 'All');
     });
   }
   return (
     <section className="main">
-      <input
-        placeholder="search countries"
-        type="text"
-        value={query}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
-      />
-      <select value={continent} onChange={(e) => setContinent(e.target.value)}>
-        <option value="">All</option>
-        <option value="africa">Africa</option>
-        <option value="asia">Asia</option>
-        <option value="europe">Europe</option>
-        <option value="north-america">North America</option>
-        <option value="south-america">South America</option>
-        <option value="antarctica">Antarctica</option>
-        <option value="australia">Australia</option>
-      </select>
-
+      <div className="search">
+        <input
+          placeholder="search Countries"
+          type="text"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+        <select value={continent} onChange={(e) => setContinent(e.target.value)}>
+          <option value="[]">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">North America</option>
+          <option value="South America">South America</option>
+          <option value="Antarctica">Antarctica</option>
+          <option value="Australia">Australia</option>
+        </select>
+      </div>
       <div className="cards">
         {filterCountries().map((item) => {
           return <CountryCard key={item.id} {...item} />;
